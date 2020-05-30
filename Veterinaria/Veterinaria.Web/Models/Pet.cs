81c78e175deb5b7fb90ef40ad9ad44197b39d386
Bernal03 <61256617+Bernal03@users.noreply.github.com>
2020-05-30 14:39:12 -0700

    Update Admin and client

--- Veterinaria/Veterinaria.Web/Models/Pet.cs
+++ Veterinaria/Veterinaria.Web/Models/Pet.cs
@@ -34,6 +34,7 @@ namespace Veterinaria.Web.Models
         [Required]
         [Display(Name = "Altura")]
         public decimal Height { get; set; }
+        public string ImgUrl { get; set; }
         public Owner Owner { get; set; }
 
         public ICollection<Consult> Consults { get; set; }
